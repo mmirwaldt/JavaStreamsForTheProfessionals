a8a89668e7a014f47bf14a796e460023af3dfa5d
mmirwaldt <michael.mirwaldt@gmail.com>
2023-01-23 08:46:48 +0100

    improved example ParallelStream_02_RangeClosed_LargerSteps

--- src/main/java/net/mirwaldt/streams/ParallelStream_02_RangeClosed_LargerSteps.java
+++ src/main/java/net/mirwaldt/streams/ParallelStream_02_RangeClosed_LargerSteps.java
@@ -2,17 +2,16 @@ package net.mirwaldt.streams;
 
 import net.mirwaldt.streams.util.SumUtil;
 
-import java.util.Arrays;
 import java.util.stream.IntStream;
 
-public class ParallelStream_02_Range_LargerSteps {
+public class ParallelStream_02_RangeClosed_LargerSteps {
     public static void main(String[] args) {
-        long largerStepsParallelStream = IntStream
-                .rangeClosed(1, 10_000_000)
+        long countMultiplesOf3AndSumOfDigits12 = IntStream
+                .rangeClosed(1, 200_000_000)
                 .map(i -> i * 3) // in steps of 3, i.e. 3, 6, 9, ...
                 .filter(i -> SumUtil.sumOfDigits(i) == 12)
                 .parallel()
                 .count();
-        System.out.println(largerStepsParallelStream);
+        System.out.println(countMultiplesOf3AndSumOfDigits12);
     }
 }
