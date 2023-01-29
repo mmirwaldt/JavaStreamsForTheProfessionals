41b8c10b4c4bd679f7292381a89239d676421e18
mmirwaldt <michael.mirwaldt@gmail.com>
2023-01-30 00:52:58 +0100

    added example for OOM in a parallel stream

--- src/main/java/net/mirwaldt/streams/ParallelStream_09_OOMandUnordered.java
+++ src/main/java/net/mirwaldt/streams/ParallelStream_09_OOMandUnordered.java
@@ -16,6 +16,15 @@ public class ParallelStream_09_OOMandUnordered {
                 .parallel()
                 .count());
 
+        // Doesn't lead to any OOM because skip() is called AFTER limit()
+        System.out.println(IntStream
+                .iterate(0, i -> i + 1)
+                .limit(10_000_000)
+                .skip(2)
+                .filter(i -> sumOfDigits(i) <= 20)
+                .parallel()
+                .count());
+
         // Warning: leads to OOM!
         // See https://stackoverflow.com/q/75012814/2023533 for more information
         System.out.println(IntStream
