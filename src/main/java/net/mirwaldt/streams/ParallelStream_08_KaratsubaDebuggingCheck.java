88162928cc4d5e72063a413c004ed6cd99c8264f
mmirwaldt <michael.mirwaldt@gmail.com>
2023-02-04 14:57:28 +0100

    improved example to check whether Karatsuba is used

--- src/main/java/net/mirwaldt/streams/ParallelStream_08_KaratsubaDebuggingCheck.java
+++ src/main/java/net/mirwaldt/streams/ParallelStream_08_KaratsubaDebuggingCheck.java
@@ -2,12 +2,13 @@ package net.mirwaldt.streams;
 
 import java.math.BigInteger;
 
-public class ParallelStream_11_KaratsubaDebuggingCheck {
+public class ParallelStream_08_KaratsubaDebuggingCheck {
+    // This example enables you to check whether karatsuba is used in debugging more with break points in BigInteger
     public static void main(String[] args) {
         BigInteger bigInt1 = BigInteger.ONE;
-        bigInt1 = bigInt1.shiftLeft(2528);
+        bigInt1 = bigInt1.shiftLeft(2560);
         BigInteger bigInt2 = BigInteger.ONE;
-        bigInt2 = bigInt2.shiftLeft(2528); // ~10^843
+        bigInt2 = bigInt2.shiftLeft(2560); // ~10^843
         System.out.println(bigInt1.multiply(bigInt2).bitLength());
     }
 }
